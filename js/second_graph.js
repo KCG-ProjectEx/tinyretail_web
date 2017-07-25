acb7d72347d5c98548dc977ba08fc8c4ff8a9576
gcyagi <s.yagi@gemcook.com>
2017-07-25 10:31:59 +0900

    graph type changed.

--- js/second_graph.js
+++ js/second_graph.js
@@ -1,7 +1,7 @@
 var ctx = document.getElementById("secondGraphCanvas");
 
 var myChart = new Chart(ctx, {
-    type: 'polarArea',
+    type: 'bar',
     data: {
       labels: ['0-9','10-19','20-29','30-39','40-49','50-59','60-69','70-79','80-89','90-99'],
       datasets:[{
