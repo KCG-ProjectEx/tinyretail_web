1a37a9fc9dcc4755842e87fe49882583ac77d30c
Shinsaku Yagi <jaming.jaming.g@gmail.com>
2017-10-03 14:16:54 +0900

    debug

--- js/update_graph.js
+++ js/update_graph.js
@@ -8,8 +8,9 @@ function myChart_UPDATE()
 //    var date = target.innerText.replace( ///g, '-' );
     var date = target.innerText.split( '/' ).join( '-' );
 
-    var datas = getCurrentDateData(date);
+    var datas = JSON.parse(getCurrentDateData(date));
 console.log(datas);
+
     myChart_favor.update(); // グラフの再描画
 }
 
