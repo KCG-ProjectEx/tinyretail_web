616177d213827ed593a0fee64c9df94b551502e5
gcyagi <s.yagi@gemcook.com>
2017-10-03 11:05:34 +0900

    debug中

--- js/update_graph.js
+++ js/update_graph.js
@@ -4,7 +4,9 @@ function myChart_UPDATE()
     var target = document.getElementById("output-date");
     // 受け取った日付データを表示する(ここから日付データを使ってください)
     target.innerText = document.forms.form_dete.get_dete.value;
-console.log(target);
+
+ary_timeline_men = ["2","3","2","4","5","7","9","5","2","4","3","3"];
+
     for(i=0;i<5;i++){
         for(j=0;j<12;j++){
             // グラフデータを更新
@@ -12,7 +14,6 @@ console.log(target);
         }
     }
     myChart_favor.update(); // グラフの再描画
-
 }
 
 // フォームにカレンダーの表示 (id="datepicker"で使用可能)
