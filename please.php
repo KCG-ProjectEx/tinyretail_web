47e8bb094268d823c1787346047ebdc6eadc99e4
Tomohiro <12.26tomo.aoki@gmail.com>
2017-10-03 11:09:51 +0900

    好感度分析　仮完成

---
+++ please.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once('ModelBase.php');
+
+// DB接続情報設定 (本来はここに置くべきでは無い処理)
+$connInfo = array(
+    'host'     => 'localhost',
+    'dbname'   => 'gakusai',
+    'dbuser'   => 'root',
+    'password' => ''
+);
+ModelBase::setConnectionInfo($connInfo );
+
+
+class Favor extends ModelBase
+{
+    protected $name = "hvc_p2";
+
+    public function getList()
+    {
+        $sql = sprintf('SELECT * FROM %s' , $this->name);
+        $stmt = $this->pdoIns->query($sql);
+        // $stmt->bindValue(':cart_id', $cartId);
+        $rows = $stmt->fetchAll();
+        return $rows;
+    }
+}
+$favor = new Favor();
+$ret = $favor->getList();
+
+echo "<pre>";
+var_dump($ret);
+echo "</pre>";
+?>
