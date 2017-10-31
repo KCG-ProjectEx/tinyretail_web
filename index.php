edb63d0bd0e277b4373c59162f3a3c0ac2bb9333
PrivatelyYagi <jaming.jaming.g@gmail.com>
2017-10-31 11:29:03 +0900

    fix gender graph

--- index.php
+++ index.php
@@ -37,9 +37,9 @@ try{
   $sql = 'SELECT * FROM hvc_p2 INNER JOIN sex ON hvc_p2.sex_id = sex.sex_id';
 
   foreach ($dbh->query($sql,PDO::FETCH_ASSOC) as $row) {
-    if($row['sex_val'] == 'man'){
+    if($row['sex_val'] == '男'){
       $count_men++;
-    }elseif($row['sex_val'] == 'women'){
+    }elseif($row['sex_val'] == '女'){
       $count_ladies++;
     }else{
       $count_other++;
