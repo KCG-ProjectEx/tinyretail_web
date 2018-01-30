0801dc11cd9d82a23c86158bb900774db79ad154
yamadanagamoto <yamadanagamoto@example.com>
2018-01-30 17:59:51 +0900

    flag

--- tomatrix.php
+++ tomatrix.php
@@ -1,25 +1,34 @@
-
 <?php
-    $dsn = "mysql:host=localhost;dbname=tinyretail;charset=utf8"; // Database name
-    $user = "root";           // Databese User Name
-    $password = "mysql0001";        // Database Password
-    $dbh = new PDO($dsn, $user, $password);
-
-    $sql= "SELECT * FROM julius order by desc limit 1";
-    $result = mysql_query($sql);
-    
-    while($row = mysql_fetch_assoc($result)){
-        $str = $row['favor'];
+require_once('ModelBase.php');
+// DB接続情報設定 (本来はここに置くべきでは無い処理)
+$connInfo = array(
+    'host'     => 'localhost',
+    'dbname'   => 'tinyretail',
+    'dbuser'   => 'root',
+    'password' => 'mysql0001'
+    //'password' => ''
+);
+ModelBase::setConnectionInfo($connInfo );
+class Tomatrix extends ModelBase
+{
+    protected $name = "julius";
+    public function getFavor()
+    {
+        $sql = sprintf("SELECT favor FROM %s order by date desc limit 1", $this->name);
+        $stmt = $this->query($sql);
+        return $stmt;
     }
-    echo $str;
-    $result->close();
-    if(strcmp($str,"positive")){
+}
+    $tomatrix = new Tomatrix();
+    $row = $tomatrix->getFavor();
+    $str = $row[0]['favor'];
+//    var_dump($str);
+    if($str == "positive"){
         echo 'w';
-    }elseif(strcmp($str,"negative")){
-        echo 'c';
-    }elseif(strcmp($str,"null")){
+    }elseif($str == "negative"){
+        echo 'q';
+    }elseif($str == "null"){
         echo 's';
     }else{
         echo 'b';
     }
-?> 
