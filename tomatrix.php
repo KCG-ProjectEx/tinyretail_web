e50f49f9565da1c4d93cc8685d4a919559ba5665
PrivatelyYagi <jaming.jaming.g@gmail.com>
2018-01-30 17:25:53 +0900

    fix tomatrix

--- tomatrix.php
+++ tomatrix.php
@@ -1,17 +1,58 @@
 
 <?php
+
+require_once('ModelBase.php');
+
+// DB接続情報設定 (本来はここに置くべきでは無い処理)
+$connInfo = array(
+    'host'     => 'localhost',
+    'dbname'   => 'tinyretail',
+    'dbuser'   => 'root',
+    'password' => 'mysql0001'
+    //'password' => ''
+);
+ModelBase::setConnectionInfo($connInfo );
+
+class Tomatrix extends ModelBase
+{
+    protected $name = "julius";
+
+    public function getFavor()
+    {
+        $sql = sprintf("SELECT favor FROM %s order by desc limit 1", $this->name);
+        $stmt = $this->query($sql);
+        return $stmt;
+    }
+}
+
+    $tomatrix = new Tomatrix();
+
+    $row = $tomatrix->getFavor();
+    var_dump($row);
+    $str = $row['favor'];
+
+    if(strcmp($str,"positive")){
+        echo 'w';
+    }elseif(strcmp($str,"negative")){
+        echo 'c';
+    }elseif(strcmp($str,"null")){
+        echo 's';
+    }else{
+        echo 'b';
+    }
+
+/*
     $dsn = "mysql:host=localhost;dbname=tinyretail;charset=utf8"; // Database name
     $user = "root";           // Databese User Name
     $password = "mysql0001";        // Database Password
     $dbh = new PDO($dsn, $user, $password);
-
     $sql= "SELECT * FROM julius order by desc limit 1";
-    $result = mysql_query($sql);
-    
+    $stmt = $dbh->prepare($sql);
+
     while($row = mysql_fetch_assoc($result)){
         $str = $row['favor'];
     }
-    echo '$str';
+    var_dump($str);
     $result->close();
     if(strcmp($str,"positive")){
         echo 'w';
@@ -23,3 +64,4 @@
         echo 'b';
     }
 ?> 
+*/
